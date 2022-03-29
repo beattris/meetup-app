0702234a15b300fd1dae7410b9e9e77dee17d5bc
Beattris <beatriceebirim@gmail.com>
2022-03-29 23:21:19 +0100

    worked with params for SSG data fetching

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -12,4 +12,19 @@ function MeetupDetails() {
   );
 }
 
+export async function getStaticProps(context){
+  const meetupId = context.params.meetupId;
+  return{
+    props: {
+      meetupData: {
+        image: "https://images.pexels.com/photos/280221/pexels-photo-280221.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        id: meetupId,
+        title: "DevFest",
+        address: "123 street",
+        description: "A description about the DevFest"
+      }
+    }
+  }
+}
+
 export default MeetupDetails;
